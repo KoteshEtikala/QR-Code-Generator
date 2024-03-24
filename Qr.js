96f69e5b4100816770bb64c92be2a4ed85cff1e9
KoteshEtikala <163092937+KoteshEtikala@users.noreply.github.com>
2024-03-24 22:51:09 +0530

    Add files via upload

--- Qr.js
+++ Qr.js
@@ -4,15 +4,22 @@ let form=document.querySelector("form")
 form.addEventListener("submit",(e)=>{
     e.preventDefault()
     let input=document.querySelector("#url").value
+    let section=document.querySelector("#qr-container")
+    let h1=document.querySelector("h1")
+    console.dir(section)
     console.log(input)
-    // let image=document.querySelector("#image")
-    // console.dir(image)
     if(input==""){
         alert("Please Enter The URL")
     }else{
         let qr=`https://chart.googleapis.com/chart?cht=qr&chs=400x400&chl=${input}`
         image.setAttribute("src",qr);
         body.append(image);
+        section.style.backgroundColor="pink"
+        section.style.boxShadow="6px 6px 6px pink"
+        h1.style.color="white"
+       
+        
+        
 
     }
     
